9f7c0c4a42a1d9f4474412e5842fc115a68aa879
Michael Moser <Michael.Moser11@web.de>
2025-03-23 12:45:55 +0100

    update dailies for obsidian plugin

--- plugins/obsidian_nvim.lua
+++ plugins/obsidian_nvim.lua
@@ -37,5 +37,17 @@ return {
     min_chars = 2,
 	},
 	disable_frontmatter = true,
+	-- dailies
+	daily_notes = {
+		folder = "3 Daily",
+		-- Optional, if you want to change the date format for the ID of daily notes.
+		date_format = "%Y%m%d",
+		-- Optional, if you want to change the date format of the default alias of daily notes.
+		alias_format = "%B %-d, %Y",
+		-- Optional, default tags to add to each new daily note created.
+		default_tags = { "daily-notes" },
+		-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+		template = "Templates/dailyToDo.md"
+	},
   },
 }
