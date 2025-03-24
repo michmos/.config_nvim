783fc02fd0738829dbe413ac0454d0b95e9d7e68
Michael Moser <Michael.Moser11@web.de>
2025-03-24 18:45:45 +0100

    obsidian: add check checkbox keybind

--- plugins/obsidian_nvim.lua
+++ plugins/obsidian_nvim.lua
@@ -57,6 +57,30 @@ return {
 		return path:with_suffix(".md")
 	end,
 
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["gf"] = {
+		action = function()
+			return require("obsidian").util.gf_passthrough()
+		end,
+		opts = { noremap = false, expr = true, buffer = true },
+		},
+		-- Toggle check-boxes.
+		["<leader>ch"] = {
+		action = function()
+			return require("obsidian").util.toggle_checkbox()
+		end,
+		opts = { buffer = true },
+		},
+		-- Smart action depending on context, either follow link or toggle checkbox.
+		["<cr>"] = {
+		action = function()
+			return require("obsidian").util.smart_action()
+		end,
+		opts = { buffer = true, expr = true },
+		}
+	},
+
 	-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
   	completion = {
     -- Set to false to disable completion.
